7ca2d48a01db4ce13dbccbcf6c2bc0bc03b55dea
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 14:26:32 +0000

    Dev tool initial commit

---
+++ lib/armadillo_view_tool.rb
@@ -0,0 +1,6 @@
+require "armadillo_view_tool/version"
+require "armadillo_view_tool/renderer"
+
+module ArmadilloViewTool
+  # Your code goes here...
+end
---
+++ lib/armadillo_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ArmadilloViewTool
+  VERSION = "0.1.0"
+end
